da607b72926f0e7707df210d6f342c40accf808b
vtait257 <4l3xvu.tu4n4nh@gmail.com>
2017-05-29 23:08:39 +0700

    Help
    Done

---
+++ Form_Main.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void đăngNhậpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Form_Login lg = new Form_Login();
+            lg.Show();
+        }
+
+        private void nhânViênToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Form_NhanVien nv = new Form_NhanVien();
+            nv.Show();
+        }
+
+        private void kháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_KhachHang kh = new Form_KhachHang();
+            kh.Show();
+        }
+
+        private void nhàCungCấpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_NhaCungCap ncc = new Form_NhaCungCap();
+            ncc.Show();
+        }
+
+        private void hàngHóaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_MatHang mh = new Form_MatHang();
+            mh.Show();
+        }
+
+        private void tìmKiếmNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_TImKIem_NhanVien nv = new Form_TImKIem_NhanVien();
+            nv.Show();
+        }
+
+        private void tìmKiếmKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_TImKiem_KhachHang kh = new Form_TImKiem_KhachHang();
+            kh.Show();
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trợGiúpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmHelp help = new frmHelp();
+            this.Hide();
+            help.Show();
+        }
+    }
+}
